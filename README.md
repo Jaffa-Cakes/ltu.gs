451cd2b3aca6ddacccce08e19a8ccc8c66517390
Jedd Dryden <40693089+Jaffa-Cakes@users.noreply.github.com>
2022-07-28 11:34:01 +1000

    1.0.0 (#1)
    * Initial
    
    Includes:
    - Member Signups
    - Social Media
    - Email Address
    - Logo
    
    * Create README.md
    
    * Create LICENSE.md
    
    * Renamed License File

---
+++ README.md
@@ -0,0 +1,29 @@
+# The (LTUGS) La Trobe University Gaming Society's Website
+
+This is a [Next.js](https://nextjs.org/) project and is built into a static set of files so that a `node.js` server is not required when live.
+
+## Development
+
+To run the development server:
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+## Production
+
+To build the production version of the website:
+
+```bash
+npm run build
+```
+
+The built version will be stored in the `out` directory.
+
+## Contribution
+
+Feel free to contrinue to the repository and help out the society if there is anything you'd like to add!
+
+Note that this project is intended to be a static website without any dynamic content due to our simple requirements.
---
+++ pages/index.tsx
@@ -0,0 +1,85 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>LTU Gaming Society</title>
+        <meta name="description" content="The official La Trobe Gaming Society. Home to both casual and competitive gaming alongside social gatherings and events." />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+
+      <main className={styles.main}>
+        <Image src="/images/logo.png" className={styles.logo} alt="LTUGS - La Trobe Gaming Society Logo" width="200" height="200"></Image>
+        <h1 className={styles.title}>
+          LTU Gaming Society
+        </h1>
+
+        <p className={styles.description}>
+          Casual and Competitive Video Game and Table Top Club
+        </p>
+
+        <div className={styles.grid}>
+          <a href="https://discord.gg/K9gCCey" className={styles.card}>
+            <h2>Discord &rarr;</h2>
+            <p>For latest updates and events.</p>
+          </a>
+
+          <a href="https://twitter.com/LTU_GS/" className={styles.card}>
+            <h2>Twitter &rarr;</h2>
+            <p>See exactly what we&apos;re thinking.</p>
+          </a>
+
+          <a
+            href="https://www.instagram.com/ltugamingsociety/"
+            className={styles.card}
+          >
+            <h2>Instagram &rarr;</h2>
+            <p>See photos and videos from our most recent events!</p>
+          </a>
+
+          <a
+            href="https://www.facebook.com/ltugs"
+            className={styles.card}
+          >
+            <h2>Facebook &rarr;</h2>
+            <p>
+              Follow our page and to see us in your feed!
+            </p>
+          </a>
+
+          <a
+            href="https://forms.gle/jiPHBjTcxB2JgKJP9"
+            className={styles.card}
+          >
+            <h2>Student Member Signup &rarr;</h2>
+            <p>If you are a La Trobe Student, register for the club here.</p>
+          </a>
+
+          <a
+            href="https://forms.gle/joH2uJ5UcSNWVY8W7"
+            className={styles.card}
+          >
+            <h2>Associate Member Signup &rarr;</h2>
+            <p>
+              If you are not a La Trobe Student, register for the club here.
+            </p>
+          </a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <p><a href="mailto:ltugs@latrobe.edu.au">ltugs@latrobe.edu.au</a></p>
+      </footer>
+    </div>
+  )
+}
+
+export default Home
